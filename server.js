47e02adf1f6ba0e0a11d51ec5815fa39550c9bcf
Jan Donyada <14304570+jandonyada@users.noreply.github.com>
2023-10-13 15:50:39 +0800

    pm-room-13-oct

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { Pool } = require('pg');
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
@@ -51,6 +52,52 @@ app.get("/work", function (req, res) {
 // about page
 app.get("/interest", function (req, res) {
   res.render("pages/interest");
+});
+
+const pool = new Pool({
+  user: 'admin_ogp',
+  host: 'demo-database-instance-1.cu4ey6grsfnr.ap-southeast-2.rds.amazonaws.com',
+  database: 'main',
+  password: 'padmin1234',
+  port: 5432, // PostgreSQL default port
+});
+
+app.get('/status', (req, res) => res.send({ status: "I'm up and running" }));
+
+app.listen(port, () => console.log(`Node.js Application is listening on port ${port}!`));
+
+app.post('/insert', (req, res) => {
+  const { username, email, age } = req.query;
+
+  if (username && email && age) {
+    console.log('Received an insert call');
+    const insertQuery = 'INSERT INTO users (username, email, age) VALUES ($1, $2, $3)';
+    const values = [username, email, age];
+
+    pool.query(insertQuery, values, (err, result) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send({ username, email, age });
+      }
+    });
+  } else {
+    console.log('Something went wrong, Missing a parameter');
+    res.status(400).send('Missing parameters');
+  }
+});
+
+app.get('/list', (req, res) => {
+  console.log('Received a list call');
+  const selectQuery = 'SELECT * FROM users';
+
+  pool.query(selectQuery, (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(result.rows);
+    }
+  });
 });
 
 app.listen(3000);
